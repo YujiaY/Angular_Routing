1129dac5e886b90c72c1ce52f64838847b949adf
Jack <jack.yujia.yuan@gmail.com>
2018-03-01 21:16:10 +0800

    Added server resolver.

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import {ActivatedRoute, Params, Router} from "@angular/router";
+import {ActivatedRoute, Data, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-server',
@@ -16,18 +16,27 @@ export class ServerComponent implements OnInit {
               private  router: Router) { }
 
   ngOnInit() {
-    const id = +this.route.snapshot.params['id'];
-    this.server = this.serversService.getServer(id);
-    this.route.params
+    this.route.data
       .subscribe(
-        (params: Params) => {
-          this.server = this.serversService.getServer(+params['id']);
+        (data: Data) => {
+          this.server = data['serverRes'];
         }
-      )
+      );
+    //comment out coz changing to Resolver
+    // const id = +this.route.snapshot.params['id'];
+    // this.server = this.serversService.getServer(id);
+    // this.route.params
+    //   .subscribe(
+    //     (params: Params) => {
+    //       this.server = this.serversService.getServer(+params['id']);
+    //     }
+    //   )
   }
   onEdit() {
     this.router.navigate(['edit'],
-      {relativeTo: this.route, queryParamsHandling: 'preserve'});
+      {relativeTo: this.route,
+        queryParamsHandling: 'preserve'
+      });
 
   }
 
